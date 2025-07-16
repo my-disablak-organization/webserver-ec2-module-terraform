f629e565ad137d09d21ec4b35baa01d456b8d354
Max Verchynskyi <disablaksteam@gmail.com>
2025-07-16 15:17:16 +0300

    fixes

--- infrastructure/100-outputs.tf
+++ infrastructure/100-outputs.tf
@@ -13,3 +13,24 @@ output "db_ip" {
     instance.id => instance.private_ip
   }
 }
+
+// remove
+output "instance_http_ids" {
+  value = [for i in aws_instance.http : i.id]
+}
+// remove
+output "instance_db_ids" {
+  value = [for i in aws_instance.db : i.id]
+}
+
+output "vpc_cidr" {
+  value = aws_vpc.terraform.cidr_block
+}
+
+output "subnet_http_cidr" {
+  value = aws_subnet.http.cidr_block
+}
+
+output "subnet_db_cidr" {
+  value = aws_subnet.db.cidr_block
+}
