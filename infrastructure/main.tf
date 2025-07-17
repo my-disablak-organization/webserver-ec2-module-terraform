a015762a26860ac1d1f0a22acb023ca218054f74
Max Verchynskyi <disablaksteam@gmail.com>
2025-07-17 11:11:20 +0300

    little fix

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "w11t1-terraform-states"
+    bucket       = "w11t1-terraform-state"
     key          = "state"
     region       = "us-east-1"
     use_lockfile = true
